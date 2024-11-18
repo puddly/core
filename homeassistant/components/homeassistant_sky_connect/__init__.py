586519078bfee28743730f443e80b66e3c26ca0f
puddly <32534428+puddly@users.noreply.github.com>
2024-11-18 17:37:33 -0500

    homeassistant_sky_connect: Unload the config entry when the device is gone

--- homeassistant/components/homeassistant_sky_connect/__init__.py
+++ homeassistant/components/homeassistant_sky_connect/__init__.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 import logging
+import pathlib
 
+from homeassistant.components import usb
 from homeassistant.components.homeassistant_hardware.const import (
     EVENT_FIRMWARE_INFO_LOADED,
 )
@@ -13,13 +15,37 @@ from homeassistant.components.homeassistant_hardware.util import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Event, HomeAssistant, callback
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up a Home Assistant SkyConnect config entry."""
-    await hass.config_entries.async_forward_entry_setups(config_entry, ["update"])
+    device = pathlib.Path(config_entry.data["device"])
+
+    if not await hass.async_add_executor_job(device.exists):
+        raise ConfigEntryNotReady(
+            translation_domain=DOMAIN, translation_key="device_not_plugged_in"
+        )
+
+    @callback
+    def async_port_event(
+        added_devices: set[usb.USBDevice], removed_devices: set[usb.USBDevice]
+    ) -> None:
+        _LOGGER.debug("Added devices: %r", added_devices)
+        _LOGGER.debug("Removed devices: %r", removed_devices)
+
+        for device in removed_devices:
+            if device.device == config_entry.data["device"]:
+                hass.config_entries.async_schedule_reload(config_entry.entry_id)
+                break
+
+    config_entry.async_on_unload(
+        usb.async_register_port_event_callback(hass, async_port_event)
+    )
 
     @callback
     def event_state_change_listener(event: Event[EventFirmwareInfoLoaded]) -> None:
@@ -39,6 +65,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     config_entry.async_on_unload(
         hass.bus.async_listen(EVENT_FIRMWARE_INFO_LOADED, event_state_change_listener)
     )
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["update"])
+
     return True
 
 
